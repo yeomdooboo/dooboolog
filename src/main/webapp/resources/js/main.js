fdebd0fe03e87fa35c0b4b1fce035f61453a08b6
yeomdooboo <animal.yum@gmail.com>
2018-03-02 01:22:16 +0900

    fix nav bar on top

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -7,4 +7,21 @@ doobooRequire([
         console.log(value);
     });
     console.log('dooboo require js!');
+
+    $(document).ready(function() {
+        var navBar = $(".nav").offset();
+        $(window).scroll(function() {
+            var docScrollY = $(document).scrollTop();
+            var barThis = $(".nav");
+
+            if (docScrollY > navBar.top) {
+                barThis.addClass("nav_fix");
+            } else {
+                barThis.removeClass("nav_fix");
+            }
+        });
+    });
+
 });
+
+
