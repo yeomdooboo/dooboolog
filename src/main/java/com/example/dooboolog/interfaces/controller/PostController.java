28697edac0bc26757d30579280c79442a30aaf30
Dooboo(염수진) <honey@coupang.com>
2018-03-02 18:31:24 +0900

    add jpa configuration

--- src/main/java/com/example/dooboolog/interfaces/controller/PostController.java
+++ src/main/java/com/example/dooboolog/interfaces/controller/PostController.java
@@ -7,21 +7,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping("/post")
 @Controller
 public class PostController {
 	@Autowired
 	PostService postService;
 
-	@RequestMapping("/post/list")
+	@RequestMapping("/list")
 	public ModelAndView postList() {
 		ModelAndView mav = new ModelAndView("post/list");
 		postService.addEssentialDataModelAndView(mav);
 		return mav;
 	}
 
-	@RequestMapping("/post/add")
+	@RequestMapping("/add")
 	@ResponseBody
 	public String addPost() {
 		return postService.addPosts();
+	}
+
+	@RequestMapping("/new")
+	public String newPost() {
+		return "/post/postForm";
 	}
 }
